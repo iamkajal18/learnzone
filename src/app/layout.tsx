ab273941fd3c158135a85f89546297912c81aa3f
iamkajal18 <152779714+iamkajal18@users.noreply.github.com>
2025-07-15 13:27:35 +0530

    refactor: update import paths to relative paths in layout and adjust tsconfig for module resolution

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,12 +3,12 @@
 
 import "./globals.css";
 import { ReactNode, useEffect, useState } from "react";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 import { SessionProvider } from "next-auth/react";
 import toast, { Toaster } from "react-hot-toast";
 import { ClerkProvider } from "@clerk/nextjs"; // Only if you were using Clerk
-import { ThemeProvider } from "@/components/ThemeContext";
+import { ThemeProvider } from "../components/ThemeContext";
 
 export default function Layout({ children }: { children: ReactNode }) {
   const [isFirstVisit, setIsFirstVisit] = useState(false);
